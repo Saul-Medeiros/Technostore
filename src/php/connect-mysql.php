9903f9c051820a406ad0d89964730f19dba1ac82
Saul Medeiros <saulmedeiros2017@gmail.com>
2023-06-11 12:34:46 -0300

    Removendo projeto para correção de bugs

--- src/php/connect-mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conexao = mysqli_connect("127.0.0.1", "root", "", "technostore");
-
-if (mysqli_connect_errno()) {
-    die("Falha de Conexão com o MySQL: " . mysqli_connect_error());
-}
-?>
